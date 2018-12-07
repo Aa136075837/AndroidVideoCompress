1caae753a0c9726c87eb99fe50989e6d4d272a5e
杨金波 <ex-yangjb001@cmrh.com>
2018-12-07 18:11:23 +0800

    init

--- app/src/main/java/com/mac/androidvideocompress/MainActivity.kt
+++ app/src/main/java/com/mac/androidvideocompress/MainActivity.kt
@@ -10,6 +10,8 @@ import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.bumptech.glide.Glide
+import com.mac.compressjava.videocompressor.SiliCompressor
 import com.mac.compressjava.videocompressor.VideoCompressAsyncTask
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
@@ -17,6 +19,7 @@ import java.io.File
 class MainActivity : AppCompatActivity(), VideoCompressAsyncTask.CompressListener {
     val TAG: String = "MainActivity"
     val REQUEST_VIDEO_CODE: Int = 0x125
+    val REQUEST_IMAGE_CODE: Int = 0x321
     val REQUEST_PERMISSION_STORAGE = 0x4562
     override fun onSuccess(compressedFile: File) {
         Log.e(TAG, "压缩完成")
@@ -42,6 +45,11 @@ class MainActivity : AppCompatActivity(), VideoCompressAsyncTask.CompressListene
             val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, REQUEST_VIDEO_CODE)
         }
+
+        image_left.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_IMAGE_CODE)
+        }
     }
 
     fun checkPermission(permission: String): Boolean {
@@ -55,26 +63,49 @@ class MainActivity : AppCompatActivity(), VideoCompressAsyncTask.CompressListene
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_VIDEO_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                val uri = data?.data
-                val cursor = contentResolver.query(uri, null, null, null, null)
-                if (cursor != null) {
-                    if (cursor.moveToFirst()) {
+        if (resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    if (requestCode == REQUEST_VIDEO_CODE) {
                         val id = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID))
                         val videoPath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA))
-                        val f = File(getStoragePath() + "/videos")
-
-                        if (!f.exists()) {
-                            f.mkdir()
-                        }
+                        val size = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE))
+                        val f = compressFilePath("/videos")
                         compress(File(videoPath), f.path)
+                    } else if (requestCode == REQUEST_IMAGE_CODE) {
+                        val id = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID))
+                        val imagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                        val size = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE))
+                        val f = compressFilePath("/images")
+                        val compressedImagePath = SiliCompressor.with(this).compress(imagePath, f)
+                        val compressedSize = File(compressedImagePath).length()
+
+                        val genThumbImgFile = ImageUtils.genThumbImgFile(imagePath, 1024 * 1024)
+
+                        Glide.with(this).load(imagePath).into(image_left)
+                        Glide.with(this).load(compressedImagePath).into(image_right)
+                        Glide.with(this).load(genThumbImgFile.absolutePath).into(image_center)
+
+                        tvLeft.text = (size / 1024f / 1024f).toString() + " MB"
+                        tvRight.text = (compressedSize / 1024f / 1024f).toString() + " MB"
+                        tvCenter.text = (genThumbImgFile.length() / 1024f / 1024f).toString() + " MB"
                     }
-                    cursor.close()
                 }
+                cursor.close()
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun compressFilePath(type: String): File {
+        val f = File(getStoragePath() + type)
+
+        if (!f.exists()) {
+            f.mkdir()
+        }
+        return f
     }
 
     fun compress(sourceFile: File, compressPath: String) {
